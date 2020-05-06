20d50fdac47e5cc213068c5f7d607010ecd1b138
OAndersonQL <5332788+coldspire@users.noreply.github.com>
2020-05-05 23:15:55 -0400

    Update from lame heroes

--- toh-examples/projects/A-TestingModule/src/app/mock-heroes.ts
+++ toh-examples/projects/A-TestingModule/src/app/mock-heroes.ts
@@ -1,14 +1,11 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Kambei' },
+  { id: 12, name: 'Heihachi' },
+  { id: 13, name: 'Gorobei' },
+  { id: 14, name: 'Kyuzo' },
+  { id: 15, name: 'Shinchiroji' },
+  { id: 16, name: 'Katsushiro' },
+  { id: 17, name: 'Kikuchiyo' },
 ];
