d18a4e91f86f7a2b5383f723883450ae5cbb83a9
Owen Anderson <44033462+OAndersonQL@users.noreply.github.com>
2020-05-05 22:32:54 -0400

    Add stuff (#1)
    * add applications
    
    * add TOH code with passing tests
    
    Co-authored-by: Owen Anderson <5332788+coldspire@users.noreply.github.com>

---
+++ toh-examples/projects/A-TestingModule/src/app/app.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent,
+        MessagesComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'Tour of Heroes'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Tour of Heroes');
+  });
+});
